9813cb354bce0ad5517622d039d326f300d01049
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2021-07-13 13:03:56 +0100

    Add block and cat components and try to get different size blocks
    Co-authored-by: chunzg <chunzhang53@gmail.com>

---
+++ src/Block.svelte
@@ -0,0 +1,21 @@
+<script>
+	// export let name;
+    // import 
+    // let heig
+</script>
+
+<div></div>
+<style>
+	div {
+		border: solid 1px black;
+		height: 75px;
+		width: 200px;
+        background-color: red;
+	}
+
+	/* @media (min-width: 640px) {
+		div {
+			max-width: none;
+		}
+	} */
+</style>
---
+++ src/Cat.svelte
@@ -0,0 +1,24 @@
+<script>
+	// export let name;
+</script>
+
+<!-- <div> -->
+	<img src='cat.svg' alt='a cat'>
+<!-- </div> -->
+
+<style>
+    img {
+        height: 150px;
+    }
+	/* div {
+		border: solid 1px black;
+		height: 200px;
+		width: 240px;
+	} */
+
+	/* @media (min-width: 640px) {
+		div {
+			max-width: none;
+		}
+	} */
+</style>
